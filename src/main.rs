b2456ce2f2d18edb5780c6a6f93830c85e78e3f7
Souvlaki42 <souvlaki420@gmail.com>
2025-05-28 21:43:33 +0300

    feat: infinite REPL!

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
-
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    println!("{}: command not found", input.trim())
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        println!("{}: command not found", input.trim())
+    }
 }
