0a961e3f6a05d50d01a2d8a1fdbaabc47c51410a
Souvlaki42 <souvlaki420@gmail.com>
2025-05-28 22:07:26 +0300

    feat: simple tokenizer and parser. Implemented exit command.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-#[allow(unused_imports)]
-use std::io::{self, Write};
+use std::{
+    io::{self, Write},
+    process,
+};
 
 fn main() {
     loop {
@@ -7,6 +9,22 @@ fn main() {
         io::stdout().flush().unwrap();
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
-        println!("{}: command not found", input.trim())
+
+        let mut inside_string = false;
+        let cmd: Vec<&str> = input
+            .split(|c| {
+                if c == '\'' || c == '"' {
+                    inside_string = !inside_string;
+                }
+                !inside_string && c == ' '
+            })
+            .map(|token| token.trim())
+            .collect();
+
+        if cmd.len() == 2 && cmd[0] == "exit" {
+            process::exit(cmd[1].parse().unwrap())
+        } else {
+            println!("{}: command not found", input.trim());
+        }
     }
 }
