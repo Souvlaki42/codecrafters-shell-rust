f1fd37ccacc75c4ed73b02cbed70bb9c6dbb3c7f
Souvlaki42 <souvlaki420@gmail.com>
2025-06-07 00:35:29 +0300

    fix: fixing execution now

--- src/execution.rs
+++ src/execution.rs
@@ -36,69 +36,80 @@ pub struct CommandResult {
 }
 
 impl CommandResult {
+    // Check `if self.external` and skip `if name = BUILTINS`
     pub fn to_output(&self, out: &mut Writer) {
-        if self.external {
+        // Check external commands and return
+        if !self.external {
             return;
         }
+
         let output = self.output.clone();
         match output {
             CommandOutput::Stdout(stdout) => {
                 out.write_all(stdout.as_bytes()).unwrap();
-                out.write_all(b"\n").unwrap();
-                out.flush().unwrap();
+                out.write_all(b"\n").unwrap(); // explicit new line.
+                out.flush().unwrap(); // ensure that it is displayed
             }
             CommandOutput::StdoutAndStderr(stdout, _) => {
                 out.write_all(stdout.as_bytes()).unwrap();
-                out.write_all(b"\n").unwrap();
-                out.flush().unwrap();
+                out.write_all(b"\n").unwrap(); // explicit new line.
+                out.flush().unwrap(); // ensure that it is displayed
             }
             _ => {}
         }
     }
 
+    // Check `if self.external` and skip `if name = BUILTINS`
     pub fn to_error(&self, err: &mut Writer) {
-        if self.external {
+        // Check external commands and return
+        if !self.external {
             return;
         }
+
         let output = self.output.clone();
         match output {
             CommandOutput::Stderr(stderr) => {
                 err.write_all(stderr.as_bytes()).unwrap();
-                err.write_all(b"\n").unwrap();
-                err.flush().unwrap();
+                err.write_all(b"\n").unwrap(); // explicit new line.
+                err.flush().unwrap(); // ensure that it is displayed
             }
             CommandOutput::StdoutAndStderr(_, stderr) => {
                 err.write_all(stderr.as_bytes()).unwrap();
-                err.write_all(b"\n").unwrap();
-                err.flush().unwrap();
+                err.write_all(b"\n").unwrap(); // explicit new line.
+                err.flush().unwrap(); // ensure that it is displayed
             }
             _ => {}
         }
     }
 }
 
+// Ensure writers have `dyn Write` not the explicit types.
 fn execute_external(cmd: &str, args: &Vec<String>, writers: (&Writer, &Writer)) -> CommandResult {
+    // Stdio config
     let stdout = match writers.0 {
         Writer::File(file) => Stdio::from(file.try_clone().unwrap()),
-        _ => Stdio::inherit(),
+        _ => Stdio::piped(), // Use a pipe for stdout
     };
     let stderr = match writers.1 {
         Writer::File(file) => Stdio::from(file.try_clone().unwrap()),
-        _ => Stdio::inherit(),
+        _ => Stdio::piped(), // Use a pipe for stderr
     };
-    let process = match Command::new(cmd)
-        .stdout(stdout)
-        .stderr(stderr)
-        .args(args)
-        .spawn()
-    {
+
+    // Set cmd
+    let mut command = Command::new(cmd);
+    command.stdout(stdout); // Set new stdout
+    command.stderr(stderr); // Set new stderr
+    command.args(args);
+
+    let process = match command.spawn() {
         Ok(process) => process,
         Err(e) => {
+            // Check to display the problem with the messages
+            let message = format!("Failed to spawn command '{}': {}\n", cmd, e);
+            eprintln!("Process problem: {message}");
+
             return CommandResult {
-                output: CommandOutput::Stderr(format!(
-                    "Failed to spawn command '{}': {}\n",
-                    cmd, e
-                )),
+                output: CommandOutput::Stderr(message),
                 exit_code: 1,
                 external: true,
             };
@@ -108,8 +119,11 @@ fn execute_external(cmd: &str, args: &Vec<String>, writers: (&Writer, &Writer)) 
     let output = match process.wait_with_output() {
         Ok(output) => output,
         Err(e) => {
+            let message = format!("Retrieving output error: {}\n", e);
+            eprintln!("Wait and get message problem: {message}");
+
             return CommandResult {
-                output: CommandOutput::Stderr(format!("Retrieving output error: {}\n", e)),
+                output: CommandOutput::Stderr(message),
                 exit_code: 1,
                 external: true,
             };
@@ -119,8 +133,11 @@ fn execute_external(cmd: &str, args: &Vec<String>, writers: (&Writer, &Writer)) 
     let stdout = match String::from_utf8(output.stdout) {
         Ok(stdout) => stdout,
         Err(e) => {
+            let message = format!("Translating stdout error: {}\n", e);
+            eprintln!("Stdout transalation problem: {message}");
+
             return CommandResult {
-                output: CommandOutput::Stderr(format!("Translating stdout error: {}\n", e)),
+                output: CommandOutput::Stderr(message),
                 exit_code: 1,
                 external: true,
             };
@@ -129,13 +146,18 @@ fn execute_external(cmd: &str, args: &Vec<String>, writers: (&Writer, &Writer)) 
     let stderr = match String::from_utf8(output.stderr) {
         Ok(stderr) => stderr,
         Err(e) => {
+            // To test if this is the actual error we receive, println to stderr
+            let message = format!("Translating stderr error: {}\n", e);
+            eprintln!("Stderr translation problem: {message}");
+
             return CommandResult {
-                output: CommandOutput::Stderr(format!("Translating stderr error: {}\n", e)),
+                output: CommandOutput::Stderr(message),
                 exit_code: 1,
                 external: true,
             };
         }
     };
+
     let status = output.status.code().unwrap_or_default();
 
     CommandResult {
@@ -145,6 +167,7 @@ fn execute_external(cmd: &str, args: &Vec<String>, writers: (&Writer, &Writer)) 
     }
 }
 
+// The logic should still be the same from the other ones.
 pub fn execute(
     name: String,
     args: Value,
@@ -229,7 +252,7 @@ pub fn execute(
             },
             Err(_) => CommandResult {
                 output: CommandOutput::Stderr(format!(
-                    "cd: {}: No such file or directory",
+                    "cd: {}: No such file or directory\n",
                     path.to_string_lossy()
                 )),
                 exit_code: 1,
